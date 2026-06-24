11d15197a3479e68a2ab8acc7625d1d33f70b77f
jonathanfelicianne-cloud <jonathanfelicianne@gmail.com>
2026-06-24 00:20:02 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,3 @@
-export default function Home() {
-  return (
-    <main style={{
-      display: 'flex',
-      minHeight: '100vh',
-      alignItems: 'center',
-      justifyContent: 'center'
-    }}>
-      <h1>Maison Salih ça marche ! 🔥</h1>
-    </main>
-  )
-}
+   export default function Page() {
+     return <h1>Maison Salih en ligne</h1>
+   }
