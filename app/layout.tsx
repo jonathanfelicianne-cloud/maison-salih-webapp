e3e2aef7d9c596c6ae7ec350d825cb31b06ba5bd
jonathanfelicianne-cloud <jonathanfelicianne@gmail.com>
2026-06-24 01:12:01 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,4 @@
-export const metadata = {
-  title: 'Maison Salih',
-  description: 'Traiteur turc',
-}
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body>{children}</body>
